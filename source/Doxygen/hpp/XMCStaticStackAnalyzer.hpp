a28b711e185754d6d65ff00fe1c2dea296c245f9
Thomas Schneider <thomas.schneider@sys-thos.de>
2021-03-26 09:46:49 +0100

    Added missing *.hpp file

---
+++ source/Doxygen/hpp/XMCStaticStackAnalyzer.hpp
@@ -0,0 +1,26 @@
+﻿// CodeGear C++Builder
+// Copyright (c) 1995, 2018 by Embarcadero Technologies, Inc.
+// All rights reserved
+
+// (DO NOT EDIT: machine generated header) 'XMCStaticStackAnalyzer.pas' rev: 33.00 (Windows)
+
+#ifndef XmcstaticstackanalyzerHPP
+#define XmcstaticstackanalyzerHPP
+
+#pragma delphiheader begin
+#pragma option push
+#pragma option -w-      // All warnings off
+#pragma option -Vx      // Zero-length empty class member 
+#pragma pack(push,8)
+
+//-- user supplied -----------------------------------------------------------
+
+//-- forward type declarations -----------------------------------------------
+//-- type declarations -------------------------------------------------------
+//-- var, const, procedure ---------------------------------------------------
+#pragma pack(pop)
+#pragma option pop
+
+#pragma delphiheader end.
+//-- end unit ----------------------------------------------------------------
+#endif	// XmcstaticstackanalyzerHPP
